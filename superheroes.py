ae3b1b4ab13b19fc545a1d733606277804b12403
Anissa Abdel-Jelil <aabdeljelil@hds.harvard.edu>
2020-01-13 17:54:34 -0500

    add superheroes table by reading json and writing to csv

--- superheroes.py
+++ superheroes.py
@@ -1,26 +1,44 @@
 #by Anissa and Pratyusha 
-# It's cleaner to put all the imports at the beginning of the file 
+# It's cleaner to put all the imports at the beginning of 
+#the file 
 
 import json
+import csv
 
-# Reads superheroes.json (in this folder)
+# Read superheroes.json (in this folder)
 
 with open('superheroes.json', 'r') as f:
 	superheroes = json.load(f)
 	
-# prints superheroes
-
 # print(superheroes)
 # Creates an empty array called powers
-powers = []
+# powers = []
 
-# Loop thorough the members of the squad, 
-#and append the powers of each to the powers array.
-members = superheroes['members']
-for member in members: 
-	this_members_powers = member['powers']
-	powers.append(this_members_powers)
+# Write a header to the CSV file
+with open('superheroes.csv', 'w') as f:
+	writer = csv.writer(f)
+	headers = ['name', 'age', 'secretIdentity', 
+		'powers', 'squadName', 'homeTown', 'formed', 
+		'secretBase', 'active']
+	writer.writerow(headers)
+
+	# Loop thorough the members of the squad, 
+	#and append the powers of each to the powers array.
+	members = superheroes['members']
+	for member in members: 
+
+	# Define variables 
+		name = member['name']
+		age = member['age']
+		secret_identity = member['secretIdentity']
+		powers = member['powers']
+		squad_name = superheroes['squadName']
+		home_town = superheroes['homeTown']
+		formed = superheroes['formed']
+		secret_base = superheroes['secretBase']
+		active = superheroes['active']
 
 # Prints those powers to the terminal
-
-print(powers)
+		row = [name, age, secret_identity, powers, squad_name,home_town,formed,secret_base,active]
+		writer.writerow(row)
+		
